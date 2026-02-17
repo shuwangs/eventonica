f4728385457ca411316182bb458536d7e4124e65
shu.wang <60482034+shuwangs@users.noreply.github.com>
2026-02-17 22:19:50 +0000

    add db schema

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # eventonica
+
+## MVP
+- [ ] Add / Delete
+
+- [ ] Search by date
+
+- [ ] Search by category
+
+- [ ] useReducer（ actions/state）
+
+- [ ] tests + README
+
+## Nice to have
+
+- [ ]⭐ Favorite Events
+
+- [ ] Upcoming / Past Events 自动分类
+
+- [ ] Debounced search（300ms）
+
+- [ ] fuzzy match, 
+
+- [ ] Calendar view
+  
+## Component Hierarchy
+- App (State Center):  useReducer，manage all the state
+
+- SearchBar: user's input deal with  Debounced Search。
+
+- EventForm:  Add Event logic。
+
+- EventList:  Upcoming / Past , update, delete
+
+- EventCard: 
+
+---
+
+# Data model Event
+
+id: number
+
+title: string
+
+category: string
+
+event_date_time: YYYY-MM-DD-HH-MM-SS
+
+location: string
+
+description: Text
+
+is_favorite: boolean
---
+++ server/db/schema.sql
@@ -0,0 +1,35 @@
+CREATE SCHEMA IF NOT EXISTS eventsdb;
+
+-- Create tables for events
+CREATE TABLE IF NOT EXISTS eventsdb.events (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category_id INTEGER,
+    event_date_time TIMESTAMP NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    description TEXT,
+    is_favorite BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+-- Create tables for categories
+CREATE TABLE IF NOT EXISTS eventsdb.categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- add a foreign key constraint to link events to categories
+ALTER TABLE eventsdb.events 
+    ADD CONSTRAINT category_id
+    FOREIGN KEY (category_id)
+    REFERENCES eventsdb.categories(id) 
+;
+
+-- create a junction table for many-to-many relationship between events and categories
+CREATE TABLE IF NOT EXISTS eventsdb.events_categories (
+    event_id INTEGER  REFERENCES eventsdb.events(id),
+    category_id INTEGER  REFERENCES eventsdb.categories(id),
+    PRIMARY KEY (event_id, category_id)
+);
+
+
