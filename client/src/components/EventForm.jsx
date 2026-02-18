9a0205f7c048cc26fd3588663207e3ae60007ee6
shu.wang <60482034+shuwangs@users.noreply.github.com>
2026-02-18 18:31:45 +0000

    include category in eventform

--- client/src/components/EventForm.jsx
+++ client/src/components/EventForm.jsx
@@ -7,6 +7,7 @@ const EventForm = ({ eventOnSubmit }) => {
         name: "",
         event_date_time: "",
         location: "",
+        category: "",
         descriptions: ""
     });
 
@@ -19,16 +20,19 @@ const EventForm = ({ eventOnSubmit }) => {
 
     return (
         <Form>
-            <label htmlFor="event-name">Event Name</label>
+            <label htmlFor="event-name">Event Name*</label>
             <input type="text" id="event-name" name="name" value={event.name} onChange={handleOnChange} required />
 
-            <label htmlFor="event-date-time">Event Date and Time</label>
+            <label htmlFor="event-date-time"> Date and Time*</label>
             <input type="datetime-local" id="event-date-time" name="event_date_time" value={event.event_date_time} onChange={handleOnChange} required />
 
-            <label htmlFor="event-location">Event Location</label>
+            <label htmlFor="event-location"> Location* </label>
             <input type="text" id="event-location" name="location" value={event.location} onChange={handleOnChange} required />
 
-            <label htmlFor="event-descriptions">Event Descriptions</label>
+            <label htmlFor="event-category"> Category*</label>
+            <input type="text" id="event-category" name="category" value={event.category} onChange={handleOnChange} required />
+
+            <label htmlFor="event-descriptions"> Description*</label>
             <textarea id="event-descriptions" name="descriptions" value={event.descriptions} onChange={handleOnChange} required></textarea>
 
             <button type="submit">Add Event</button>
