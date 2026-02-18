9ba92837f19cbe8162db159b711e3bf0d44e3c34
shu.wang <60482034+shuwangs@users.noreply.github.com>
2026-02-18 18:32:54 +0000

    include div container and onsubmti func in eventForm

--- client/src/components/EventForm.jsx
+++ client/src/components/EventForm.jsx
@@ -19,25 +19,28 @@ const EventForm = ({ eventOnSubmit }) => {
     
 
     return (
-        <Form>
-            <label htmlFor="event-name">Event Name*</label>
-            <input type="text" id="event-name" name="name" value={event.name} onChange={handleOnChange} required />
+        <div className="event-form-container">
+            <Form className="event-form" onSubmit={handleSubmit}>
+                <label htmlFor="event-name">Event Name*</label>
+                <input type="text" id="event-name" name="name" value={event.name} onChange={handleOnChange} required />
 
-            <label htmlFor="event-date-time"> Date and Time*</label>
-            <input type="datetime-local" id="event-date-time" name="event_date_time" value={event.event_date_time} onChange={handleOnChange} required />
+                <label htmlFor="event-date-time"> Date and Time*</label>
+                <input type="datetime-local" id="event-date-time" name="event_date_time" value={event.event_date_time} onChange={handleOnChange} required />
 
-            <label htmlFor="event-location"> Location* </label>
-            <input type="text" id="event-location" name="location" value={event.location} onChange={handleOnChange} required />
+                <label htmlFor="event-location"> Location* </label>
+                <input type="text" id="event-location" name="location" value={event.location} onChange={handleOnChange} required />
 
-            <label htmlFor="event-category"> Category*</label>
-            <input type="text" id="event-category" name="category" value={event.category} onChange={handleOnChange} required />
+                <label htmlFor="event-category"> Category*</label>
+                <input type="text" id="event-category" name="category" value={event.category} onChange={handleOnChange} required />
 
-            <label htmlFor="event-descriptions"> Description*</label>
-            <textarea id="event-descriptions" name="descriptions" value={event.descriptions} onChange={handleOnChange} required></textarea>
+                <label htmlFor="event-descriptions"> Description*</label>
+                <textarea id="event-descriptions" name="descriptions" value={event.descriptions} onChange={handleOnChange} required></textarea>
 
-            <button type="submit">Add Event</button>
+                <button type="submit">Add Event</button>
 
-        </Form>
+            </Form>
+        </div>
+
     )
 }
 
