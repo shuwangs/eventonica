99e9d1cbc5f9a67ce605efe7667837cd22897772
Shu <shuw425@gmail.com>
2026-02-18 03:59:16 -0500

    add placeholders for query utils

---
+++ server/utils/queries.js
@@ -0,0 +1,30 @@
+
+
+// Manager function:
+
+const GET_USERS = `
+SELECT * FROM eventsdb.users
+ORDER BY id;
+`;
+
+const ADD_EVENT =  ``;
+
+const DELETE_EVENT = ``;
+
+const UPDATE_EVENT = ``;
+
+
+// USer and Manager shared function;
+
+const GET_EVENTS = `
+SELECT * FROM users`;
+
+const GET_EVENTS_BY_CATEGORY = ``;
+
+// User function
+
+const ADD_FAVORITE = ``;
+
+const GET_USER_FAVORITES = ``;
+
+
