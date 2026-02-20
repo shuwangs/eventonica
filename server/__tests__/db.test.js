430b3ba23550b946ac8f927b9e6c639a6bd21d70
Shu <shuw425@gmail.com>
2026-02-19 20:42:44 -0500

    test: tested and fixed the test for userService

--- server/__tests__/db.test.js
+++ server/__tests__/db.test.js
@@ -1,14 +1,14 @@
 import pool from "../db/db.js"
 
-const test = async () => {
-  try {
-    const res = await pool.query("SELECT NOW()");
-    console.log("DB Connected:", res.rows[0]);
-  } catch (err) {
-    console.error("DB Error:", err);
-  } finally {
-    await pool.end();
-  }
-};
+// const test = async () => {
+//   try {
+//     const res = await pool.query("SELECT NOW()");
+//     console.log("DB Connected:", res.rows[0]);
+//   } catch (err) {
+//     console.error("DB Error:", err);
+//   } finally {
+//     await pool.end();
+//   }
+// };
 
-test();
+// test();
--- server/__tests__/userService.test.js
+++ server/__tests__/userService.test.js
@@ -1,5 +1,5 @@
 import {describe, it, expect} from '@jest/globals';
-import * as userService from '../services/userService.js';
+import userService from '../services/userService.js';
 import pool from '../db/db.js';
 
 describe('User Service Tests  ', () => {
@@ -7,7 +7,8 @@ describe('User Service Tests  ', () => {
 
     it('should add users to the database', async () => {
         const name = "Test User";
-        const email = "test@example.com";
+        const randAttache = (Math.random()* 10).toString();
+        const email =  `test`+ randAttache + "@gmail.com";
 
         const newUser = await userService.addUser(name, email);
 
@@ -22,15 +23,25 @@ describe('User Service Tests  ', () => {
     });
 
     it('should delete a user from the database', async () => {
-        const users = await userService.getAllUsers();
-        if (users.length === 0) {
-            throw new Error("No users to delete");
-        }
-        
-        const userToDelete = users[0];
-        const deletedUser = await userService.deleteUser(userToDelete.id);  
-        const found = users.find(u => u.id === userToDelete.id);
-        expect(deletedUser).toBeUndefined();
+
+        const name = "Delete Me";
+        const email = `del_${Math.random()}@gmail.com`;
+        const created = await userService.addUser(name, email);
+
+        const deleted = await userService.deleteUser(created.id);
+
+        expect(deleted).toBeTruthy();
+
+        expect(deleted.id).toBe(created.id);
+
+        const usersAfterDelete = await userService.getAllUsers();
+        const found = usersAfterDelete.find((u) => u.id === created.id);
+
+        expect(found).toBeUndefined();
+    });
+
+    afterAll(async () => {
+         await pool.end();
     });
 
 });
