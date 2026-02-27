6c831550524919595cd4dc8c0d9d7db958cc5d7a
Shu <shuw425@gmail.com>
2026-02-26 21:05:46 -0500

    Add mock db in the userService

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,13 @@
 import {Pool} from 'pg';
 import dotenv from 'dotenv';
-dotenv.config('../.env' );
-if (!process.env.DATABASE_URL) {
-  throw new Error("Missing DATABASE_URL in .env");
 
-}
+const env = process.env.ENV || "development";
+
+dotenv.config({path: env == "test" ? ".env.test" : ".env"})
+
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("Missing DATABASE_URL in .env");
+// }
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
